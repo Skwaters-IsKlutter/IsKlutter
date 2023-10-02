7c8b7557ba8544b29cbfe0f6985e63fae0d88771
nico <90756549+sengokus@users.noreply.github.com>
2023-10-02 17:02:21 +0800

    Added logo to landing page

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const logo = require('./resources/logo-1.png'); 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome to IsKlutter!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={logo} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +20,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 250,
+    height: 250,
   },
 });
