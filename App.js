12163f5aec002cbbd080347369c0ca0c5db197f5
nico <90756549+sengokus@users.noreply.github.com>
2023-10-02 16:26:33 +0800

    Edited App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ethot</Text>
+      <Text>Welcome to IsKlutter!</Text>
       <StatusBar style="auto" />
     </View>
   );
