905f63575a9d3a2118d19e2b546b5f41fcaff528
purrccc <persecatalan@gmail.com>
2023-10-14 19:43:11 +0800

    Created HomePage.js

---
+++ src/HomePage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    VStack,
+    Heading,
+    Image,
+    Box, 
+    Button, 
+    ButtonText, 
+    FormControl, 
+    FormControlLabel, 
+    FormControlError, 
+    FormControlErrorText, 
+    FormControlLabelText, 
+    FormControlHelper, 
+    FormControlHelperText, 
+    FormControlErrorIcon, 
+    Input, 
+    InputField
+} from '@gluestack-ui/themed';
+
+import colors from '../app/config/colors.js'
