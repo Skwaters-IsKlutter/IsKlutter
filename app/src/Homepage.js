45a76f7bbb8ae1341c366c5b127555a0af2703c2
nico <90756549+sengokus@users.noreply.github.com>
2023-10-23 11:00:23 +0800

    Fix

--- app/src/Homepage.js
+++ app/src/Homepage.js
@@ -27,39 +27,21 @@ const Tab = createBottomTabNavigator();
 
 export default function HomepagePage() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Listings" component={ListingsScreen} />
-            {/* <Tab.Screen name="Community" component={SettingsScreen} /> */}
-        </Tab.Navigator>
+        // <Tab.Navigator>
+        //     <Tab.Screen name="Listings" component={ListingsScreen} />
+        //     {/* <Tab.Screen name="Community" component={SettingsScreen} /> */}
+        // </Tab.Navigator>
         // Parent box
-        // <Box w="100%" h="100%">
-        //     {/*Search Bar*/}
-        //     <SearchHeader />
+        <Box w="100%" h="100%">
+            {/*Search Bar*/}
+            <SearchHeader />
             
-        //     <Box p="$6" w="100%" maxWidth="$96" flex={1}>
-        //         {/*Listings Label */}
-        //         <VStack space="xs" pb="$2">
-        //             <Heading lineHeight={60} fontSize="$5xl" color={colors.secondary}>Listings</Heading>
-        //         </VStack>
+            <Box p="$6" w="100%" maxWidth="$96" flex={1}>
+                {/*Listings Label */}
+                <VStack space="xs" pb="$2">
+                    <Heading lineHeight={60} fontSize="$5xl" color={colors.secondary}>Listings</Heading>
+                </VStack>
 
-<<<<<<< HEAD
-        //         {/*Listing Box Container*/}
-        //         <ScrollView>
-        //             <HStack space="xs" flexWrap="wrap">
-        //                 <ItemCard />
-        //                 <ItemCard />
-        //                 <ItemCard />
-        //                 <ItemCard />
-        //                 <ItemCard />
-        //                 <ItemCard />
-        //                 <ItemCard />
-        //                 <ItemCard />
-        //                 <ItemCard />
-        //             </HStack>
-        //         </ScrollView>
-        //     </Box>
-    
-=======
                 {/*Listing Box Container*/}
                 <ScrollView>
                     <HStack space="xs" flexWrap="wrap">
@@ -75,31 +57,6 @@ export default function HomepagePage() {
                     </HStack>
                 </ScrollView>
             </Box>
-        
-            
-
->>>>>>> parent of b34461e (Attempt at implementing bottom tab navigation)
-
-
-            // {/*Dashboard */}
-            // /* <Box top="90%" position="absolute" h="20%" width="100%" backgroundColor="$green" borderRadius={50} alignItems='center'>
-            //     <HStack space="xs" p="$5">
-            //         <Button variant="solid"  backgroundColor={colors.secondary} >
-            //             <ButtonText sx={{
-            //                 color: colors.medium
-            //             }}>Listings</ButtonText>
-            //         </Button>
-            //         <Button variant="solid"  backgroundColor={colors.secondary}>
-            //             <ButtonText sx={{
-            //                 color: colors.medium
-            //             }}>Community</ButtonText>
-            //         </Button>     
-            //         <Button variant="solid"  backgroundColor={colors.secondary}>
-            //             <ButtonText sx={{
-            //                 color: colors.medium
-            //             }}>Profile</ButtonText>
-            //         </Button>        
-            //     </HStack>   
-            // </Box> */
+        </Box>
     )
 }
