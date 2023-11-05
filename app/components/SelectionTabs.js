6012d6e3bf20b3df01393064ebf4d3b775819b81
nico <90756549+sengokus@users.noreply.github.com>
2023-11-05 20:50:35 +0800

    Added Community navigation, TODO: tabs

--- app/components/SelectionTabs.js
+++ app/components/SelectionTabs.js
@@ -15,24 +15,28 @@ import {
     TabsTab,
     Pressable
 } from '@gluestack-ui/themed';
+import { useNavigation } from '@react-navigation/native';
 import { Alert } from 'react-native';
 
 import colors from '../config/colors.js';
+import Routes from '../components/constants/Routes.js';
 
 export default function SelectionTabs({ tabTitle1, tabTitle2, tabValue1, tabValue2 }) {
+    const navigation = useNavigation();
+
     return (
         <Box>
             <Tabs bg={colors.primary} h="$10">
                 <TabsTabList>
                     {/* Tab selection */}
                     <TabsTab value="tab1">
-                        <Pressable onPress={() => Alert.alert("Alert", "This is a dummy action")}>
+                        <Pressable onPress={() => navigation.navigate(Routes.HOMEPAGE)}>
                             <TabsTabTitle color={colors.white} fontWeight="$bold">{tabTitle1}</TabsTabTitle>
                         </Pressable>
                     </TabsTab>
 
                     <TabsTab value="tab2">
-                        <Pressable onPress={() => Alert.alert("Alert", "This is a dummy action")}>
+                        <Pressable onPress={() => navigation.navigate(Routes.COMMUNITY)}>
                             <TabsTabTitle color={colors.white} fontWeight="$bold">{tabTitle2}</TabsTabTitle>
                         </Pressable>
                     </TabsTab>
