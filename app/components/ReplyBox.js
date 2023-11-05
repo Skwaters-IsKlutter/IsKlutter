20b912ab58b822eca62293281e2efb23fded9480
nico <90756549+sengokus@users.noreply.github.com>
2023-11-05 23:49:58 +0800

    Updated ReplyBox component

--- app/components/ReplyBox.js
+++ app/components/ReplyBox.js
@@ -15,21 +15,22 @@ import colors from '../config/colors.js';
 
 export default function CommentBox( {key : replyID, replyText, replyUser, replyDate, replyTime, userIcon} ) {
     return (
-        <Box p="$2" bg={colors.white}>
+        <Box p="$2" h="auto" bg={colors.white} flex={1}>
             <Box w="100%" maxWidth="$60" pb="$2">
-                <VStack space="sm" pl="21%">
-                    <Text color={colors.gray} fontSize="$lg">{replyUser}</Text>
-
-                    <HStack space ="sm">
-                        <Text color={colors.gray} fontSize="$sm">{replyDate}</Text>
-                        <Text color={colors.gray} fontSize="$sm">{replyTime}</Text>
+                <VStack space="sm" pl="21%" m={6}>
+                    <HStack space="sm">
+                        <Text color={colors.gray} fontSize="$lg" fontWeight="$extrabold">{replyUser}</Text>
+                        <HStack space="sm" pl="30%">
+                            <Text color={colors.gray} fontSize="$xs">{replyDate}</Text>
+                            <Text color={colors.gray} fontSize="$xs">{replyTime}</Text>
+                        </HStack>
                     </HStack>
                 </VStack>
 
                 <HStack space="sm" justifyContent="center" alignItems="center">
                     <Image source={userIcon} h={45} w={45} alt="icon" borderRadius={100} />
                     <Box h="$10" w="75%">
-                        <Input bg={colors.white} borderColor={colors.secondary} p={6}>
+                        <Input bg={colors.white} borderColor={colors.secondary} p={10}>
                             <Text>{replyText}</Text>
                         </Input>
                     </Box>
