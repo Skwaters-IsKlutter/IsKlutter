f05dd561f59f09c34b3a3ac4bd55a50431bac276
nico <90756549+sengokus@users.noreply.github.com>
2023-11-07 16:52:42 +0800

    Created AddListing page without image upload

--- app/components/AddListingBox.js
+++ app/components/AddListingBox.js
@@ -11,13 +11,43 @@ import {
     InputField,
 } from '@gluestack-ui/themed';
 
+import AddListingForm from './AddListingForm.js';
+
 import colors from '../config/colors.js';
 
-export default function AddListingBox( {user, listingImage, listingName, listingPrice, listingTags} ) {
+export default function AddListingBox( {user, listingImage, listingName, listingPrice, listingDescription, listingTags} ) {
     return (
-        <Box p="$2" bg={colors.white}>
+        <Box p="$2" bg={colors.medium} borderRadius={12}>
             <Box w="100%" maxWidth="$60" pb="$2">
+                <AddListingForm 
+                    listingFormLabel="Name"
+                    listingFormType="text"
+                    listingFormPlaceholder="Enter listing name"
+                    listingFormValue={listingName}
+                />
 
+                <AddListingForm 
+                    listingFormLabel="Price"
+                    listingFormType="number"
+                    listingFormPlaceholder="Enter listing price"
+                    listingFormValue={listingPrice}
+                />
+
+                <AddListingForm 
+                    listingFormLabel="Description"
+                    listingFormType="text"
+                    listingFormPlaceholder="Enter listing description"
+                    listingFormValue={listingDescription}
+                    listingFormMultiline={true}
+                    listingFormBoxHeight={100}
+                />
+
+                <AddListingForm 
+                    listingFormLabel="Tags"
+                    listingFormType="text"
+                    listingFormPlaceholder="Enter listing tags"
+                    listingFormValue={listingTags}
+                />
             </Box>
         </Box>
     )
