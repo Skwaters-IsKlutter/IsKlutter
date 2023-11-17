63f40e99503a1f41c44504c2bbc0c5883897088a
HENERALPEPITO <95832346+HENERALPEPITO@users.noreply.github.com>
2023-11-17 22:18:24 +0800

    Add files via upload
    Updated the firebase since test mode expired. Login and SignUp is okay

---
+++ Login.js
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import { Alert } from 'react-native';
+import {
+    VStack,
+    Heading,
+    Image,
+    Box,
+    Button,
+    ButtonText,
+    FormControl,
+    FormControlLabel,
+    FormControlError,
+    FormControlErrorText,
+    FormControlLabelText,
+    FormControlHelper,
+    FormControlHelperText,
+    FormControlErrorIcon,
+    Input,
+    InputField
+} from '@gluestack-ui/themed';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../config/firebase";
+import { useNavigation } from '@react-navigation/native';
+import colors from '../config/colors.js';
+import Routes from '../components/constants/Routes.js';
+
+
+export default function LoginPage() {
+    const navigation = useNavigation();
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+      const onHandleLogin = () => {
+        if (email !== '' && password !== '') {
+          signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+              console.log('Login success');
+              navigation.navigate(Routes.HOMEPAGE);
+            })
+            .catch((err) => {
+              Alert.alert('Login error', 'Email and password are not correct');
+            });
+        }
+      };
+
+    return (
+        // Parent box
+        <Box w="100%" h="100%" justifyContent="center" alignItems="center">
+            {/* Logo */}
+            {/* <VStack w="100%" h="$10" pb="$4" justifyContent="center" alignItems="center">
+                <Image source={ require("../assets/img/icon.png") } h={100} w={100} alt="logo" />
+            </VStack> */}
+
+            {/* Form control for login */}
+            <Box
+                p="$6"
+                w="100%"
+                maxWidth="$96"
+            >
+                {/* Heading */}
+                <VStack space="xs" pb="$12">
+                    <Heading lineHeight={60} fontSize="$5xl">Welcome back</Heading>
+                    <Heading lineHeight={30} fontSize="$2xl">Sign in to IsKlutter now.</Heading>
+                </VStack>
+
+                {/* Email input */}
+                <VStack space="xl" py="$3">
+                    <FormControl size="md">
+                        <FormControlLabel mb="$2">
+                            <FormControlLabelText>Email</FormControlLabelText>
+                        </FormControlLabel>
+                        <Input w="100%">
+                            <InputField
+                                type="email"
+                                placeholder="Enter email"
+                                value={email}
+                                onChangeText={(text) => setEmail(text)}
+                            />
+                        </Input>
+                    </FormControl>
+                </VStack>
+
+                {/* Password input */}
+                <VStack space="xl" py="$2">
+                    <FormControl size="md">
+                        <FormControlLabel mb="$2">
+                            <FormControlLabelText>Password</FormControlLabelText>
+                        </FormControlLabel>
+                        <Input w="100%">
+                            <InputField
+                                type="password"
+                                placeholder="Enter password"
+                                value={password}
+                                onChangeText={(text) => setPassword(text)}
+                            />
+                        </Input>
+                    </FormControl>
+                </VStack>
+
+                {/* Submit button */}
+                <VStack space="lg" pt="$4">
+                    <Button size="sm" backgroundColor={colors.primary} onPress={(onHandleLogin)}>
+                        <ButtonText>Sign In</ButtonText>
+                    </Button>
+                </VStack>
+            </Box>
+
+            {/* Go to sign up */}
+            <Box flexDirection="row" top={600} position="absolute">
+                <Button variant="solid" m="$7" size="sm" backgroundColor={colors.secondary} onPress={() => navigation.navigate(Routes.SIGNUP)}>
+                    <ButtonText sx={{
+                        color: colors.medium
+                    }}>Don't have an account yet? Sign up</ButtonText>
+                </Button>
+            </Box>
+        </Box>
+    )
+}
---
+++ Signup.js
@@ -0,0 +1,154 @@
+import React, { useState } from 'react';
+import {
+    VStack,
+    Heading,
+    Image,
+    Box, 
+    Button, 
+    ButtonText, 
+    FormControl, 
+    FormControlLabel, 
+    FormControlError, 
+    FormControlErrorText, 
+    FormControlLabelText, 
+    FormControlHelper, 
+    FormControlHelperText, 
+    FormControlErrorIcon, 
+    Input, 
+    InputField
+} from '@gluestack-ui/themed';
+import { useNavigation } from '@react-navigation/native';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../config/firebase';
+import colors from '../config/colors.js';
+import Routes from '../components/constants/Routes.js';
+
+export default function SignupScreen() {
+    const navigation = useNavigation();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [retypePassword, setRetypePassword] = useState(''); 
+    const [error, setError] = useState(null);
+
+    const handleSignup = async () => {
+        if (email !== '' && password !== '') {
+            try {
+                await createUserWithEmailAndPassword(auth, email, password);
+                console.log('Signup success');
+            } catch (err) {
+                console.error('Signup error:', err.message);
+                // You can use Alert.alert here if it's available in your environment
+            }
+        }
+    };
+
+    return (
+        // Parent box
+        <Box w="100%" h="100%" justifyContent="center" alignItems="center">
+            {/* Logo */}
+            {/* <VStack w="100%" h="$10" pb="$4" justifyContent="center" alignItems="center">
+                <Image source={ require("../assets/img/icon.png") } h={100} w={100} alt="logo" />
+            </VStack> */}
+
+            {/* Form control for login */}
+            <Box
+                p="$6"
+                w="100%"
+                maxWidth="$96"
+            >
+                {/* Heading */}
+                <VStack space="xs" pb="$12">
+                    <Heading lineHeight={60} fontSize="$5xl">New here?</Heading>
+                    <Heading lineHeight={30} fontSize="$2xl">Sign up to start decluttering.</Heading>
+                </VStack>
+
+                {/* Email input */}
+                  <VStack space="xl" py="$3">
+                                <FormControl size="md">
+                                    <FormControlLabel mb="$2">
+                                        <FormControlLabelText>Email</FormControlLabelText>
+                                    </FormControlLabel>
+                                    <Input w="100%">
+                                        <InputField
+                                            type="email"
+                                            placeholder="Enter email"
+                                            value={email}
+                                            onChangeText={(text) => setEmail(text)}
+                                        />
+                                    </Input>
+                                </FormControl>
+                            </VStack>
+
+
+                {/* Username input */}
+                <VStack space="xl" py="$3">
+                    <FormControl
+                        size="md"
+                        isDisabled={false}
+                        isInvalid={false}
+                        isReadOnly={false}
+                        isRequired={false}
+                    >
+                        <FormControlLabel mb="$2">
+                            <FormControlLabelText>Username</FormControlLabelText>
+                        </FormControlLabel>
+                        <Input w="100%">
+                            <InputField type="username" defaultValue="" placeholder="Enter username" />
+                        </Input>
+                    </FormControl>
+                </VStack>
+
+             {/* Password input */}
+                        <VStack space="xl" py="$2">
+                            <FormControl size="md">
+                                <FormControlLabel mb="$2">
+                                    <FormControlLabelText>Password</FormControlLabelText>
+                                </FormControlLabel>
+                                <Input w="100%">
+                                    <InputField
+                                        type="password"
+                                        placeholder="Enter password"
+                                        value={password}
+                                        onChangeText={(text) => setPassword(text)}
+                                    />
+                                </Input>
+                            </FormControl>
+                        </VStack>
+
+               {/* Re-enter password input */}
+            <VStack space="xl" py="$2">
+                <FormControl size="md">
+                    <FormControlLabel mb="$2">
+                        <FormControlLabelText>Re-enter Password</FormControlLabelText>
+                    </FormControlLabel>
+                    <Input w="100%">
+                        <InputField
+                            type="password"
+                            placeholder="Re-enter password"
+                            value={retypePassword}
+                            onChangeText={(text) => setRetypePassword(text)}
+                        />
+                    </Input>
+                </FormControl>
+            </VStack>
+
+            {/* Submit button */}
+            <VStack space="lg" pt="$4">
+                <Button size="sm" backgroundColor={colors.primary} onPress={handleSignup}>
+                    <ButtonText>Sign Up</ButtonText>
+                </Button>
+            </VStack>
+            </Box>
+
+            {/* Go to sign up */}
+            <Box flexDirection="row" top={800} position="absolute">
+                <Button variant="solid" m="$7" size="sm" backgroundColor={colors.secondary} onPress={() => navigation.navigate(Routes.LOGIN)}>
+                    <ButtonText sx={{
+                        color: colors.medium
+                    }}>Already have an account? Sign in</ButtonText>
+                </Button>
+            </Box>
+        </Box>
+    )
+}
