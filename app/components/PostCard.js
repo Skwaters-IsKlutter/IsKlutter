f8d8ce96010ca5cbe92b0fe0d1afd2a435ca617e
nico <90756549+sengokus@users.noreply.github.com>
2023-11-21 13:28:57 +0800

    Changed ListingCard chat and added icon

--- app/components/PostCard.js
+++ app/components/PostCard.js
@@ -20,15 +20,16 @@ import CommunityCommentBox from './CommunityCommentBox.js';
 export default function PostCard( { posterIcon, posterName, postDate, postContent, username} ) {
     return (
         <VStack>
-            <Box p="$3" w="100%" backgroundColor={colors.white} m={3} borderRadius={8}>
-                <HStack pb="$3" justifyContent="left">
+            <Box p={15} w="100%" backgroundColor={colors.white} borderRadius={8}>
+                <HStack pb={10} >
                     {/* <Image source={posterIcon} h={45} w={45} alt="icon" borderRadius={100}/>  */}
                     <UserAvatar username={username} userIcon={posterIcon} />
                     <Text color={colors.gray}size="sm" bold={true}>{posterName}</Text>
                     <Text color={colors.gray} size="2xs">{postDate}</Text>
                 </HStack>
 
-                <Text color="black" pb="$3" size="sm">{postContent}</Text>
+                <Text color="black" pb="$3" size="sm" m={7}>{postContent}</Text>
+
                 <CommunityCommentBox comment={() => Alert.alert("Alert", "This is a dummy action")} />
             </Box>
         </VStack>
