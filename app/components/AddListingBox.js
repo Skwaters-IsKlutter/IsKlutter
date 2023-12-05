49e9bb3ba186459f2829270f580318f815b3b77d
HENERALPEPITO <95832346+HENERALPEPITO@users.noreply.github.com>
2023-12-05 10:58:23 +0800

    Add files via upload

--- app/components/AddListingBox.js
+++ app/components/AddListingBox.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Box,
     Text,
     VStack,
-    HStack,
+    FormControl, 
+    FormControlLabel, 
+    FormControlError, 
+    FormControlErrorText, 
+    FormControlLabelText, 
+    FormControlHelper, 
+    FormControlHelperText, 
+    FormControlErrorIcon, 
     Image,
     Button,
     ButtonText,
@@ -14,56 +21,22 @@ import {
 import AddListingForm from './AddListingForm.js';
 import AddListingImagePicker from '../components/AddListingImagePicker.js'
 import AddTags from './AddTags.js';
+import { getFirestore, addDoc, collection } from 'firebase/firestore';
+import { Alert } from 'react-native';
+import { FIREBASE_APP } from '../../config/firebase'; 
+const db = getFirestore(FIREBASE_APP);
+// Initialize Firestore
 
 import colors from '../config/colors.js';
 
-export default function AddListingBox( {user, listingImage, listingName, listingPrice, listingDescription, listingTags} ) {
+export default function AddListingBox({ user }) {
+   
+
     return (
         <Box p="$2" bg={colors.medium} borderRadius={12}>
             <Box w="100%" maxWidth="$60" pb={0}>
 
-                <AddListingImagePicker 
-                    listingFormLabel="Upload an Image"
-                /> 
-
-                <AddListingForm 
-                    listingFormLabel="Listing Title"
-                    listingFormType="text"
-                    listingFormPlaceholder="Enter listing name"
-                    listingFormValue={listingName}
-                    listingFormBoxHeight="$10"
-                />
-
-                <AddListingForm 
-                    listingFormLabel="Price"
-                    listingFormType="number"
-                    listingFormPlaceholder="Enter listing price"
-                    listingFormValue={listingPrice}
-                    listingFormBoxHeight="$10"
-                />
-
-                <AddListingForm 
-                    listingFormLabel="Description"
-                    listingFormType="text"
-                    listingFormPlaceholder="Enter listing description"
-                    listingFormValue={listingDescription}
-                    listingFormMultiline={true}
-                    listingFormBoxHeight={100}
-                />
-
-                {/* <AddListingForm 
-                    listingFormLabel="Tags"
-                    listingFormType="text"
-                    listingFormPlaceholder="Enter listing tags"
-                    listingFormValue={listingTags}
-                    listingFormBoxHeight="$10"
-                /> */}
-
-                <AddTags
-                    listingFormLabel="Tags"
-                    listingFormPlaceholder="Select a Tag"
-                />
-
+              
             </Box>
         </Box>
 
