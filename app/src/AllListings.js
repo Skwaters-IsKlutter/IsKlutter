8bd8e20b5f818527cbf880fefb2de4e515bb8b81
Pan <faferrer@up.edu.ph>
2023-12-27 12:15:17 +0800

    Listings are now displayed without image

--- app/src/AllListings.js
+++ app/src/AllListings.js
@@ -9,58 +9,44 @@ import {
     ButtonIcon,
     ButtonText,
 } from '@gluestack-ui/themed';
-import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
 import SearchHeader from '../components/SearchHeader.js';
 import ItemCard from '../components/ItemCard.js';
 import TagLabel from '../components/TagLabel.js';
-
 import colors from '../config/colors.js'
 import Routes from '../components/constants/Routes.js';
-
-// import { auth} from '../../config/firebase';
-// import { where, getFirestore, doc, getDoc, collection, query, getDocs } from 'firebase/firestore'; // Import necessary Firebase modules
-// import { FIREBASE_APP } from '../../config/firebase'; 
-// const db = getFirestore(FIREBASE_APP);
+import { collection, getDocs } from 'firebase/firestore';
+import { database } from '../../config/firebase'; // Import your Firebase configuration
 
 export default function AllListingsPage() {
     const navigation = useNavigation();
+    const [allListingsData, setAllListingsData] = useState([]);
 
-    const allListingsData = [
-        {
-            key: 1,
-            productImage: require("../../assets/img/item.jpg") ,
-            productName: "Kuromi Plush",
-            productPrice: "PHP 450",
-            productSeller: "cinnamonroll",
-            tags: [<TagLabel tagName="Toys" />],
-            toListing: () => navigation.navigate(Routes.LISTING, {item})
-        }, {
-            key: 2,
-            productImage: require("../../assets/img/item2.jpg") ,
-            productName: "UP Shirt",
-            productPrice: "PHP 250",
-            productSeller: "sassag0rl",
-            tags: [<TagLabel tagName="Clothing" />]
-        }
-    ]
+    const fetchAllListings = async () => {
+        const listingsCollection = collection(database, 'listings');
+        const listingsSnapshot = await getDocs(listingsCollection);
+
+        const listingsData = listingsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setAllListingsData(listingsData);
+    };
+
+    useEffect(() => {
+        fetchAllListings();
+    }, []);
 
     const renderAllListings = () => {
-        return allListingsData.map((item, index) =>
+        return allListingsData.map((item) => (
             <ItemCard
-                key={index}
-                productImage={item.productImage}
-                productPrice={item.productPrice}
-                productName={item.productName}
-                productSeller={item.productSeller}
-                tags={item.tags}
-                toListing={item.toListing}
-                // toListing={() => navigation.navigate(Routes.LISTING, {item})}
+                key={item.id}
+                productImage={item.listingImage || require("../../assets/img/item.jpg")}
+                productPrice={item.listingPrice}
+                productName={item.listingName}
+                productSeller={item.username}
+                toListing={() => navigation.navigate(Routes.LISTING, { item })}
             />
-        );
-    }
+        ));
+    };
 
     return (
         // Parent box
