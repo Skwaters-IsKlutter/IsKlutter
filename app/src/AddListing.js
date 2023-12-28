2084af310e936488e3815da060fff8686123b590
Pan <faferrer@up.edu.ph>
2023-12-28 10:16:25 +0800

    Added unique key to listings

--- app/src/AddListing.js
+++ app/src/AddListing.js
@@ -9,16 +9,14 @@ import {
     ButtonText,
 } from '@gluestack-ui/themed';
 import { useNavigation } from '@react-navigation/native';
-
 import SearchHeaderBack from '../components/SearchHeaderBack.js';
 import AddListingBox from '../components/AddListingBox.js';
-
-import { collection, addDoc, getDoc, doc, query, where, getDocs} from 'firebase/firestore';
+import { collection, addDoc, getDoc, doc, query, where, getDocs, setDoc} from 'firebase/firestore';
 import { storage, storageRef, uploadBytes,  database, auth } from '../../config/firebase';
 import { getDownloadURL } from 'firebase/storage';
 
 import colors from '../config/colors.js'
-import Routes from '../components/constants/Routes.js';
+
 
 export default function AddListingPage() {
     const navigation = useNavigation();
@@ -72,8 +70,12 @@ export default function AddListingPage() {
                 throw new Error('Firestore is currently offline. Please check your internet connection and try again.');
             }
 
-            const user = auth.currentUser; // User authenticated
-            const uid = user.uid;
+            //const user = auth.currentUser; // User authenticated
+            //const uid = user.uid;
+
+            // Generate a unique key for the new listing
+            const docRef = doc(collection(database, 'listings'));   
+            const newListingId = docRef.id;
 
             // Generate a unique filename using timestamp and random string
             const uniqueFilename = `${Date.now()}_${Math.random().toString(36).substring(2, 8)}`;   
@@ -97,7 +99,8 @@ export default function AddListingPage() {
             const downloadURL = await getDownloadURL(imageRef);
 
             // Add the listing data to Firestore with the image URL
-            const docRef = await addDoc(collection(database, 'listings'), {
+            await setDoc(docRef, {
+                key: newListingId,
                 username: username,
                 ...listingData,
                 listingImage: storagePath,
@@ -159,7 +162,7 @@ export default function AddListingPage() {
                             listingPrice={listingData.listingPrice}
                             listingDescription={listingData.listingDescription}
                             listingTags={listingData.listingTags}
-                            setListingData={setListingData} // Add this prop to update the state in AddListingBox
+                            setListingData={setListingData} // Prop to update the state in AddListingBox
                             />
                         </VStack>
                     </Box>
