78ba81d75ab203fc1955b09427acb549bad90038
Pan <faferrer@up.edu.ph>
2023-12-29 00:09:49 +0800

    Refactored Edit Profile Screen

--- app/components/EditProfileModal.js
+++ app/components/EditProfileModal.js
@@ -16,56 +16,51 @@ export default function EditProfileScreen({ route, navigation }) {
           Alert.alert('Error', 'New username is required.');
           return;
         }
-      
-        // Check if the new username is different from the current username
-        if (newUsername !== username) {
-          // Check if the new username is already taken
-          const usersCollection = collection(database, 'users');
-          const usernameQuery = query(usersCollection, where('username', '==', newUsername));
-          const usernameQuerySnapshot = await getDocs(usernameQuery);
-      
-          if (usernameQuerySnapshot.empty) {
-            try {
-              // Set loading to true
-              setLoading(true);
-      
-              // Update the user document in Firestore with the new profile data
-              const userQuery = query(usersCollection, where('userID', '==', userID));
-              const userQuerySnapshot = await getDocs(userQuery);
-      
-              if (!userQuerySnapshot.empty) {
-                const userDocRef = userQuerySnapshot.docs[0].ref;
-                await updateDoc(userDocRef, {
-                  username: newUsername,
-                  userBio: newBio,
-                  userProfile: newProfileName,
-                });
-      
-                // Update the state with the new username
-                route.params.setUsername(newUsername);
-      
-                // Show a success message
-                Alert.alert('Success', 'Profile updated successfully.');
-              } else {
-                console.log('User document not found.');
-                Alert.alert('Error', 'User document not found. Please try again.');
-              }
-            } catch (error) {
-              console.error('Error updating user profile:', error);
-              Alert.alert('Error', 'Failed to update user profile. Please try again.');
-            } finally {
-              // Set loading back to false
-              setLoading(false);
-      
-              // Navigate back to the Profile screen
-              navigation.navigate('Profile');
-            }
-          } else {
+
+        if (username !== newUsername) {
+            // Check if the new username is already taken
+            const usersCollection = collection(database, 'users');
+            const usernameQuery = query(usersCollection, where('username', '==', newUsername));
+            const usernameQuerySnapshot = await getDocs(usernameQuery);
+        
+            if (!usernameQuerySnapshot.empty) {
             Alert.alert('Error', 'Username already exists. Please choose a different username.');
             return;
+            }
+        }
+      
+        try {
+          // Set loading to true
+          setLoading(true);
+      
+          // Update the user document in Firestore with the new profile data
+          const usersCollection = collection(database, 'users');
+          const userQuery = query(usersCollection, where('userID', '==', userID));
+          const userQuerySnapshot = await getDocs(userQuery);
+      
+          if (!userQuerySnapshot.empty) {
+            const userDocRef = userQuerySnapshot.docs[0].ref;
+            await updateDoc(userDocRef, {
+              username: newUsername,
+              userBio: newBio,
+              userProfile: newProfileName,
+            });
+      
+            // Update the state with the new values
+            route.params.setUsername(newUsername);
+            setProfileName(newProfileName);
+            setBio(newBio);
+      
+            // Show a success message
+            Alert.alert('Success', 'Profile updated successfully.');
+          } else {
+            console.log('User document not found.');
+            Alert.alert('Error', 'User document not found. Please try again.');
           }
-        } else {
-          // The new username is the same as the current username, proceed without checking or updating
+        } catch (error) {
+          console.error('Error updating user profile:', error);
+          Alert.alert('Error', 'Failed to update user profile. Please try again.');
+        } finally {
           // Set loading back to false
           setLoading(false);
       
@@ -73,6 +68,7 @@ export default function EditProfileScreen({ route, navigation }) {
           navigation.navigate('Profile');
         }
       };
+      
       
 
   return (
