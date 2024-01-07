2ec16b265b7f4ccdd3dd53daa38440614a7359be
Pan <faferrer@up.edu.ph>
2024-01-07 15:32:23 +0800

    Added Logout Button

--- app/components/ProfileCard.js
+++ app/components/ProfileCard.js
@@ -4,6 +4,7 @@ import { useNavigation } from '@react-navigation/native';
 import { Alert } from 'react-native';
 import colors from '../config/colors.js';
 import profileImgPlaceHolder from '../../assets/img/profile-holder.jpg';
+import { auth } from '../../config/firebase';
 
 export default function ProfileCard({ userProfileImg, username, profileName, bio, userID, setProfileName, setUsername, setBio, loading }) {
   const navigation = useNavigation();
@@ -22,6 +23,16 @@ export default function ProfileCard({ userProfileImg, username, profileName, bio
     });
   };
 
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      navigation.navigate('Login');
+    } catch (error) {
+      console.error('Error logging out:', error);
+      // Handle logout error if needed
+    }
+  };
+
   return (
     <Box bgColor="white" p={20} borderRadius={5}>
       {/* Avatar*/}
@@ -37,7 +48,7 @@ export default function ProfileCard({ userProfileImg, username, profileName, bio
 
       <VStack space="xs" pb="$2" py="$3">
         <HStack justifyContent="space-between" alignItems="center">
-          <Heading pr="$12" pt="$1.5" pl="$1" fontSize={30} color={colors.primary}>
+          <Heading pt="$1.5" pl="$1" fontSize={30} color={colors.primary}>
             {profileName}
           </Heading>
           <Button variant="solid" size="sm" backgroundColor={colors.primary} borderRadius={20} onPress={handleEditProfile}>
@@ -51,9 +62,17 @@ export default function ProfileCard({ userProfileImg, username, profileName, bio
           <Text>Loading...</Text>
         ) : (
           <>
+            <HStack justifyContent="space-between" alignItems="center">
             <Heading px="$10" pl="$1" fontSize={20} color={colors.black} pt="0" pb="0">
               {`@${username}`}
             </Heading>
+            <Button variant="solid" size="sm" backgroundColor={colors.gray} borderRadius={20} onPress={handleLogout}>
+              <ButtonText color={colors.white} fontSize="$sm">
+                Logout
+              </ButtonText>
+            </Button>
+            </HStack>
+            
 
             <Text px="$10" pl="$1" fontSize={15} color={colors.gray} pt="0" pb="0">
               {bio}
