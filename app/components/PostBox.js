c9c41901beb3fcb0b20022dff7e02362dca69672
Pan <faferrer@up.edu.ph>
2024-01-09 11:57:37 +0800

    current user icon display in post box

--- app/components/PostBox.js
+++ app/components/PostBox.js
@@ -1,22 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import {
-    Box,
-    Text,
-    VStack,
-    HStack,
-    Image,
-    Button,
-    ButtonText,
-    Input,
-    InputField,
-} from '@gluestack-ui/themed';
+// PostBox.js
 
-import UserAvatar from './Avatar.js';
+import React, { useEffect, useState } from 'react';
+import { Box, Text, VStack, HStack, Button, ButtonText, Input, InputField, Image } from '@gluestack-ui/themed';
 import colors from '../config/colors.js';
 import { getFirestore, addDoc, collection, getDocs, query, where } from 'firebase/firestore';
 import { FIREBASE_APP } from '../../config/firebase';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-
+import { getAuth } from 'firebase/auth';
 
 const db = getFirestore(FIREBASE_APP);
 const auth = getAuth();
@@ -30,84 +19,80 @@ const generateRandomId = (length = 10) => {
   return randomId;
 };
 
-export default function PostBox({ post, posterUser, posterIcon }) {
-    const [listingDescription, setListingDescription] = useState('');
-    const [username, setUsername] = useState('');
-  
-    useEffect(() => {
-      const fetchUserData = async () => {
-        try {
-          if (!auth || !auth.currentUser) {
-            setTimeout(fetchUserData, 1000);
-            return;
-          }
-  
-          const user = auth.currentUser;
-          const userCollection = collection(db, 'users');
-          const querySnapshot = await getDocs(query(userCollection, where('userID', '==', user.uid)));
-  
-          querySnapshot.forEach((doc) => {
-            setUsername(doc.data().username); // Assuming 'username' field exists in the user document
-          });
-        } catch (error) {
-          console.error('Error fetching user data:', error.message);
-        }
-      };
-  
-      fetchUserData();
-    }, []);
-  
-    const postForum = async () => {
+export default function PostBox({ post, userProfileImg }) {
+  console.log("userProfileImg in PostBox", userProfileImg);
+  const [listingDescription, setListingDescription] = useState('');
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    const fetchUserData = async () => {
       try {
-        if (username) {
-          // Add data to Firestore collection 'forum'
-          const docRef = await addDoc(collection(db, 'forum'), {
-            key: generateRandomId(), // Assuming generateRandomId is defined
-            description: listingDescription,
-            username: username,
-          });
-    
-          // Clear input fields after posting
-          setListingDescription('');
-          alert('Posted');
-          console.log('Document written with ID: ', docRef.id);
-          console.log('Generated ID: ', docRef.id); // Display the generated ID
-        } else {
-          // Handle case where username is not fetched yet
-          console.error('Username not fetched');
+        if (!auth || !auth.currentUser) {
+          setTimeout(fetchUserData, 1000);
+          return;
         }
+
+        const user = auth.currentUser;
+        const userCollection = collection(db, 'users');
+        const querySnapshot = await getDocs(query(userCollection, where('userID', '==', user.uid)));
+
+        querySnapshot.forEach((doc) => {
+          setUsername(doc.data().username);
+        });
       } catch (error) {
-        console.error('Error adding document: ', error);
+        console.error('Error fetching user data:', error.message);
       }
     };
-    
 
-    return (
-        <Box  bgColor={colors.white} p={20}>
-            <HStack space="sm" justifyContent="space-evenly" alignItems="center">
-                <UserAvatar username={posterUser} userIcon={posterIcon} />
+    fetchUserData();
+  }, []);
 
-                <Input bg={colors.white} borderColor={colors.secondary} h={75} w="65%" zIndex={0}>
-                    <InputField
-                        multiline={true}
-                        size="md"
-                        placeholder="Write a post..."
-                        value={listingDescription}
-                        onChangeText={(text) => setListingDescription(text)}
-                    />
-                </Input>
+  const postForum = async () => {
+    try {
+      if (username) {
+        const docRef = await addDoc(collection(db, 'forum'), {
+          key: generateRandomId(),
+          description: listingDescription,
+          username: username,
+        });
 
-                <Button
-                    variant="solid"
-                    size="sm"
-                    bg={colors.secondary}
-                    borderRadius={8}
-                    onPress={postForum}
-                    ml={3}
-                >
-                    <Text color={colors.white} fontSize="$sm">Post</Text>
-                </Button>
-            </HStack>
-        </Box>
-    );
+        setListingDescription('');
+        alert('Posted');
+        console.log('Document written with ID: ', docRef.id);
+        console.log('Generated ID: ', docRef.id);
+      } else {
+        console.error('Username not fetched');
+      }
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
+  };
+
+  return (
+    <Box bgColor={colors.white} p={20}>
+      <HStack space="sm" justifyContent="space-evenly" alignItems="center">
+        <Image
+          source={userProfileImg ? { uri: userProfileImg } : require('../../assets/img/profile-holder.jpg')}
+          style={{ width: 50, height: 50, borderRadius: 25 }}
+          alt = "user profile"
+        />
+
+        <Input bg={colors.white} borderColor={colors.secondary} h={75} w="65%" zIndex={0}>
+          <InputField
+            multiline={true}
+            size="md"
+            placeholder="Write a post..."
+            value={listingDescription}
+            onChangeText={(text) => setListingDescription(text)}
+          />
+        </Input>
+
+        <Button variant="solid" size="sm" bg={colors.secondary} borderRadius={8} onPress={postForum} ml={3}>
+          <Text color={colors.white} fontSize="$sm">
+            Post
+          </Text>
+        </Button>
+      </HStack>
+    </Box>
+  );
 }
--- app/components/PostCard.js
+++ app/components/PostCard.js
@@ -1,46 +1,50 @@
 import React from 'react';
 import {
-    Box,
-    VStack,
-    HStack,
-    Button,
-    ButtonText,
-    Heading,
-    Text,
-    Image,
-    Pressable
+  Box,
+  VStack,
+  HStack,
+  Button,
+  ButtonText,
+  Heading,
+  Text,
+  Image,
+  Pressable
 } from '@gluestack-ui/themed';
 import { Alert } from 'react-native';
-
-import UserAvatar from './Avatar.js';
 
 import colors from '../config/colors.js';
 import CommunityCommentBox from './CommunityCommentBox.js';
 
+export default function PostCard({ username, userProfileImg, description, toIndividualPost }) {
+    console.log(userProfileImg); 
+  return (
+    <Pressable onPress={toIndividualPost}>
+      <VStack>
+        <Box
+          backgroundColor={colors.white}
+          borderRadius={5}
+          width={370}
+          ml={10}
+          mt={10}
+        >
+          <HStack space="sm" alignItems="center" p={5} m={5}>
+          <Image
+            source={userProfileImg ? { uri: userProfileImg } : require('../../assets/img/profile-holder.jpg')}
+            style={{ width: 50, height: 50, borderRadius: 25 }}
+            alt="User Avatar"
+            />
+            <Text color={colors.secondary} size="md" bold={true}>
+              {username}
+            </Text>
+          </HStack>
 
-export default function PostCard( { username, description, toIndividualPost} ) {
+          <Text color="black" pb="$3" size="sm" ml="$3" mt={2}>
+            {description}
+          </Text>
 
-    return (
-        <Pressable onPress={toIndividualPost} >
-            <VStack>
-                <Box 
-                    backgroundColor={colors.white} 
-                    borderRadius={5} 
-                    width={370}
-                    ml={10}
-                    mt={10}>
-
-                    <HStack space="sm" alignItems="center" p={5} m={5}>
-                        <UserAvatar/> 
-                        <Text color={colors.secondary}size="md" bold={true}>{username}</Text>
-                        {/* <Text color={colors.gray} size="2xs">{postDate}</Text> */}
-                    </HStack>
-
-                    <Text color="black" pb="$3" size="sm" ml="$3" mt={2}>{description}</Text>
-
-                    {/* <CommunityCommentBox comment={() => Alert.alert("Alert", "This is a dummy action")} /> */}
-                </Box>
-            </VStack>
-        </Pressable>
-    )
+          {/* Additional components */}
+        </Box>
+      </VStack>
+    </Pressable>
+  );
 }
