b9bbe39797e33753441b414e2af3199c9243a504
Pan <faferrer@up.edu.ph>
2024-04-02 22:28:35 +0800

    User will only now have default img after signup

--- app/src/Signup.js
+++ app/src/Signup.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Text, Image, TouchableOpacity } from 'react-native';
 import {
     VStack,
     Heading,
@@ -14,111 +13,87 @@ import {
 } from '@gluestack-ui/themed';
 import { useNavigation } from '@react-navigation/native';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { getFirestore, addDoc, collection, query, where, getDocs } from 'firebase/firestore';
-import { getStorage, ref as storageRef, uploadBytes, getDownloadURL } from 'firebase/storage';
-import Routes from '../components/constants/Routes.js';
-import * as ImagePicker from 'expo-image-picker'; // Import ImagePicker for selecting profile picture
-import { auth } from '../../config/firebase';
+import { auth, firestore } from '../../config/firebase'; // Import Firebase authentication and firestore
 import colors from '../config/colors.js';
+import Routes from '../components/constants/Routes.js';
 
-const db = getFirestore();
+const db = firestore();
 
 export default function SignupScreen() {
     const navigation = useNavigation();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [retypePassword, setRetypePassword] = useState('');
     const [username, setUsername] = useState('');
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
-    const [profileImage, setProfileImage] = useState(null); // State to store selected profile image
 
     const isUsernameUnique = async (username) => {
-        const q = query(collection(db, 'users'), where('username', '==', username));
-        const usernameSnapshot = await getDocs(q);
-        return usernameSnapshot.empty;
+        const usersRef = firestore().collection('users');
+        const querySnapshot = await usersRef.where('username', '==', username).get();
+        return querySnapshot.empty;
     };
 
     const handleSignup = async () => {
         try {
             setLoading(true);
             setError(null);
-    
-            if (!(email && password && username && password === retypePassword)) {
+
+            if (email && password && username && password === retypePassword) {
+                const uniqueUsername = await isUsernameUnique(username);
+
+                if (!email.includes('@up.edu.ph')) {
+                    setError('Please use a valid email address with @up.edu.ph domain.');
+                    setLoading(false);
+                    return;
+                }
+
+                if (!uniqueUsername) {
+                    setError('Username is already taken. Please choose a different one.');
+                } else {
+                    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+                    const defaultImgURL = "https://firebasestorage.googleapis.com/v0/b/isklutterfinal.appspot.com/o/profileImages%2Fprofile-holder.jpg?alt=media&token=244f47ba-8aa6-4284-9d4c-149a60310820";
+
+                    await addDoc(collection(db, 'users'), {
+                        email: email,
+                        userBio: "I have no interesting info.",
+                        userID: userCredential.user.uid,
+                        userProfile: username,
+                        userProfileImg: defaultImgURL,
+                        username: username,
+                    });
+
+                    console.log('User account created successfully with ID:', userCredential.user.uid);
+                    navigation.navigate(Routes.LOGIN);
+                }
+            } else {
                 setError('Please fill in all fields and make sure passwords match.');
-                return;
             }
-
-            if (!email.includes('@up.edu.ph')) {
-                setError('Please use a valid email address with @up.edu.ph domain.');
-                return;
-            }
-
-            const uniqueUsername = await isUsernameUnique(username);
-            if (!uniqueUsername) {
-                setError('Username is already taken. Please choose a different one.');
-                return;
-            }
-    
-            const userResponse = await createUserWithEmailAndPassword(auth, email, password);
-            console.log('User UID:', userResponse.user.uid);
-
-            let imageUrl = '';
-            if (profileImage) {
-                const storage = getStorage();
-                const imageRef = storageRef(storage, `profileImages/${userResponse.user.uid}`);
-                const response = await fetch(profileImage.assets[0].uri);
-                const blob = await response.blob();
-
-                const uploadTask = uploadBytes(imageRef, blob);
-                const snapshot = await uploadTask;
-
-                imageUrl = await getDownloadURL(snapshot.ref);
-            }
-
-            const userData = {
-                email: email,
-                userBio: '',
-                userID: userResponse.user.uid,
-                userProfile: username,
-                userProfileImg: imageUrl,
-                username: username,
-            };
-
-            const userDocRef = await addDoc(collection(db, 'users'), userData);
-            console.log('Document written with ID:', userDocRef.id);
-
-            navigation.navigate(Routes.LOGIN);
         } catch (error) {
-            console.error("Error", error);
-            setError('An error occurred. Please try again.');
+            console.error('Error', error);
+            setError('An error occurred while signing up. Please try again.');
         } finally {
             setLoading(false);
         }
     };
 
-    const handleChooseProfileImage = async () => {
-        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-        if (status !== 'granted') {
-            setError('Permission Denied: Camera roll permission is required to upload profile picture.');
-            return;
-        }
-
-        const result = await ImagePicker.launchImageLibraryAsync();
-        if (!result.canceled) {
-            setProfileImage(result);
-        }
-    };
-
     return (
         <Box w="100%" h="100%" justifyContent="center" alignItems="center">
-            <Box p="$6" w="100%" height={400} backgroundColor={colors.primary} borderBottomEndRadius={100} borderBottomLeftRadius={100}>
+            <Box
+                p="$6"
+                w="100%"
+                height={400}
+                backgroundColor={colors.primary}
+                borderBottomEndRadius={100}
+                borderBottomLeftRadius={100}
+            >
                 <VStack space="xs" mt={90} alignItems="center">
                     <Heading lineHeight={60} fontSize="$5xl" color={colors.white}>New here?</Heading>
                     <Heading lineHeight={30} fontSize="$2xl" color={colors.white}>Sign up to start decluttering.</Heading>
                 </VStack>
             </Box>
-            <Box p="$6" w="100%" maxWidth="$96" top={-180}>
+            <Box p="$6" w="100%" maxWidth="$96 " top={-180}>
                 <Box backgroundColor={colors.white} p="$2" borderRadius={10}>
                     <VStack space="xl" py="$3" p="$2" pt="$5">
                         <FormControl size="md">
@@ -126,7 +101,12 @@ export default function SignupScreen() {
                                 <FormControlLabelText color={colors.secondary}>Email</FormControlLabelText>
                             </FormControlLabel>
                             <Input w="100%">
-                                <InputField type="email" placeholder="Enter email" value={email} onChangeText={(text) => setEmail(text)} />
+                                <InputField
+                                    type="email"
+                                    placeholder="Enter email"
+                                    value={email}
+                                    onChangeText={(text) => setEmail(text)}
+                                />
                             </Input>
                         </FormControl>
                     </VStack>
@@ -136,7 +116,13 @@ export default function SignupScreen() {
                                 <FormControlLabelText color={colors.secondary}>Username</FormControlLabelText>
                             </FormControlLabel>
                             <Input w="100%">
-                                <InputField type="username" placeholder="Enter username" value={username} onChangeText={(text) => setUsername(text)} />
+                                <InputField
+                                    type="username"
+                                    defaultValue=""
+                                    placeholder="Enter username"
+                                    value={username}
+                                    onChangeText={(text) => setUsername(text)}
+                                />
                             </Input>
                         </FormControl>
                     </VStack>
@@ -146,7 +132,12 @@ export default function SignupScreen() {
                                 <FormControlLabelText color={colors.secondary}>Password</FormControlLabelText>
                             </FormControlLabel>
                             <Input w="100%">
-                                <InputField type="password" placeholder="Enter password" value={password} onChangeText={(text) => setPassword(text)} />
+                                <InputField
+                                    type="password"
+                                    placeholder="Enter password"
+                                    value={password}
+                                    onChangeText={(text) => setPassword(text)}
+                                />
                             </Input>
                         </FormControl>
                     </VStack>
@@ -156,18 +147,14 @@ export default function SignupScreen() {
                                 <FormControlLabelText color={colors.secondary}>Re-enter Password</FormControlLabelText>
                             </FormControlLabel>
                             <Input w="100%">
-                                <InputField type="password" placeholder="Re-enter password" value={retypePassword} onChangeText={(text) => setRetypePassword(text)} />
+                                <InputField
+                                    type="password"
+                                    placeholder="Re-enter password"
+                                    value={retypePassword}
+                                    onChangeText={(text) => setRetypePassword(text)}
+                                />
                             </Input>
                         </FormControl>
-                    </VStack>
-                    <VStack space="lg" pt="$4">
-                        <TouchableOpacity onPress={handleChooseProfileImage}>
-                            {profileImage ? (
-                                <Image source={{ uri: profileImage.assets[0].uri }} style={styles.image} />
-                            ) : (
-                                <Text style={styles.text}>Choose Profile Picture</Text>
-                            )}
-                        </TouchableOpacity>
                     </VStack>
                     <VStack space="lg" pt="$4">
                         <Button size="sm" backgroundColor={colors.primary} onPress={handleSignup} disabled={loading}>
@@ -177,22 +164,16 @@ export default function SignupScreen() {
                 </Box>
             </Box>
             <Box flexDirection="row" top={700} position="absolute">
-                <Button variant="solid" m="$7" size="sm" backgroundColor={colors.secondary} onPress={() => navigation.navigate(Routes.LOGIN)}>
+                <Button
+                    variant="solid"
+                    m="$7"
+                    size="sm"
+                    backgroundColor={colors.secondary}
+                    onPress={() => navigation.navigate(Routes.LOGIN)}
+                >
                     <ButtonText sx={{ color: colors.white }}>Already have an account? Sign in</ButtonText>
                 </Button>
             </Box>
         </Box>
     );
 }
-
-const styles = {
-    text: {
-        color: colors.black,
-        fontSize: 16,
-    },
-    image: {
-        width: 150,
-        height: 150,
-        borderRadius: 75,
-    },
-};
