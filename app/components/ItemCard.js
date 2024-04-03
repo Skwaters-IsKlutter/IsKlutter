dc405e2f82f9637d8f8de5d8df215435e023508c
Pan <faferrer@up.edu.ph>
2024-04-03 21:21:49 +0800

    Optimized ItemCard.js

--- app/components/ItemCard.js
+++ app/components/ItemCard.js
@@ -2,38 +2,31 @@ import React from 'react';
 import { Box, VStack, HStack, Heading, Text, Image, Pressable } from '@gluestack-ui/themed';
 import colors from '../config/colors.js';
 
-export default function ItemCard({ productImage, productPrice, productName, productSeller, sellerID, toListing, tags }) {
+export default function ItemCard({ productImage, productPrice, productName, productSeller, toListing, tags }) {
     const isImageUrl = typeof productImage === 'string';
 
     return (
         <Pressable onPress={toListing}>
             <Box bg={colors.white} borderRadius={10} width={150} maxHeight={256} m="2%" flex={1} overflow="hidden">
                 <Box p="$2">
+                    {isImageUrl ? (
+                        <Image
+                            source={{ uri: productImage }}
+                            style={{ height: 100, width: '100%', borderTopLeftRadius: 5, borderTopRightRadius: 5 }}
+                            resizeMode="cover"
+                            alt="icon"
+                        />
+                    ) : (
+                        <Text>No Image Available</Text>
+                    )}
+                    <Heading fontSize="$xl" color={colors.primary}>
+                        {`PHP ${productPrice}`}
+                    </Heading>
                     <VStack space="xs" p={0}>
-                        {isImageUrl && (
-                            <Image
-                                source={{ uri: productImage }}
-                                style={{ height: 100, width: '100%'}}
-                                resizeMode="cover"
-                                alt="icon"
-                                borderTopLeftRadius={5}
-                                borderTopRightRadius={5}
-                            />
-                        )}
-                        {!isImageUrl && (
-                            <Text>No Image Available</Text>
-                        )}
-                        <Heading fontSize="$xl" color={colors.primary}>
-                            {`PHP ${productPrice}`}
-                        </Heading>
-                    </VStack>
-
-                    <VStack space="xs" p={0}>
-                        <Text fontSize="$md" fontWeight="$bold">{productName}</Text>
+                        <Text fontSize="$md" fontWeight="bold">{productName}</Text>
                         <Text fontSize="$sm" color={colors.gray}>{productSeller}</Text>
                     </VStack>
-                  
-                    <HStack space="sm"  flexWrap="wrap">
+                    <HStack space="sm" flexWrap="wrap">
                         <Text bgColor={colors.primary} color={colors.white} borderRadius={15} p={7}>{tags}</Text>
                     </HStack>
                 </Box>
