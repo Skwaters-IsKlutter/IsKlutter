5b287f3999861d58cded96d89114e1d774165d40
Pan <faferrer@up.edu.ph>
2024-04-06 22:15:56 +0800

    Hot reload refactored using useFocusEffect

--- app/src/Community.js
+++ app/src/Community.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Box, VStack, ScrollView } from '@gluestack-ui/themed';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import PostBox from '../components/PostBox.js';
 import PostCard from '../components/PostCard.js';
-import { getFirestore, addDoc, collection, getDocs, query, where } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, query, where } from 'firebase/firestore';
 import UpperTabBar from '../components/UpperTabBar.js';
 import Routes from '../components/constants/Routes.js';
 import { FIREBASE_APP } from '../../config/firebase';
@@ -17,81 +17,68 @@ export default function CommunityPage() {
     const [username, setUsername] = useState('');
     const navigation = useNavigation();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const userCollection = collection(db, 'forum');
-                const querySnapshot = await getDocs(userCollection);
-                const userData = [];
-                querySnapshot.forEach((doc) => {
-                    const data = doc.data();
-                    const userDataObj = {
-                        key: doc.id,
-                        description: data.description,
-                        userID: data.userID,
-                        timeposted: data.timestamp,
-                    };
-                    userData.push(userDataObj);
-                });
-                setDescription(userData);
-            } catch (error) {
-                console.error('Error fetching user data:', error.message);
-            }
-        };
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                if (!auth || !auth.currentUser) {
-                    setTimeout(fetchUserData, 1000);
-                    return;
-                }
-                const currentUser = auth.currentUser;
-                const userCollection = collection(db, 'users');
-                const querySnapshot = await getDocs(query(userCollection, where('userID', '==', currentUser.uid)));
-                querySnapshot.forEach((doc) => {
-                    setUsername(doc.data().username);
-                });
-            } catch (error) {
-                console.error('Error fetching user data:', error.message);
-            }
-        };  
-        fetchUserData();
-    }, []);
-
-    const handlePostSubmission = async (postData) => {
+    const fetchUserData = async () => {
         try {
-            const newPost = { id: postData.id, ...postData };
-            setDescription(prevDescription => [newPost, ...prevDescription]);
+            if (!auth || !auth.currentUser) {
+                setTimeout(fetchUserData, 1000);
+                return;
+            }
+            const currentUser = auth.currentUser;
+            const userCollection = collection(db, 'users');
+            const querySnapshot = await getDocs(query(userCollection, where('userID', '==', currentUser.uid)));
+            querySnapshot.forEach((doc) => {
+                setUsername(doc.data().username);
+            });
         } catch (error) {
-            console.error('Error adding post:', error);
+            console.error('Error fetching user data:', error.message);
         }
-    }
-    
+    };
+
+    const fetchAllCommunityPosts = async () => {
+        try {
+            const userCollection = collection(db, 'forum');
+            const querySnapshot = await getDocs(userCollection);
+            const userData = [];
+            querySnapshot.forEach((doc) => {
+                const data = doc.data();
+                const userDataObj = {
+                    key: doc.id,
+                    description: data.description,
+                    userID: data.userID,
+                    timeposted: data.timestamp,
+                };
+                userData.push(userDataObj);
+            });
+            setDescription(userData);
+        } catch (error) {
+            console.error('Error fetching user data:', error.message);
+        }
+    };
+
+    useFocusEffect(() => {
+        fetchUserData();
+        fetchAllCommunityPosts();
+    });
 
     const renderAllCommunityPosts = () => {
-        return description.map((userData, index) => {
-            return (
-                <PostCard
-                    key={index}
-                    userId={userData.userID}
-                    posterIcon={userData.userProfileImg}
-                    description={userData.description}
-                    toIndividualPost={() => navigation.navigate(Routes.INDIVIDUALPOST, {selectedPost: userData })}
-                />  
-            );
-        });
-    }
+        return description.map((userData, index) => (
+            <PostCard
+                key={index}
+                userId={userData.userID}
+                posterIcon={userData.userProfileImg}
+                description={userData.description}
+                toIndividualPost={() => navigation.navigate(Routes.INDIVIDUALPOST, { selectedPost: userData })}
+            />
+        ));
+    };
 
     return (
         <Box w="100%" h="100%">
-            <UpperTabBar 
+            <UpperTabBar
                 pageTitle={"Community"}
             />
             <ScrollView>
-                <PostBox onPostSubmit={handlePostSubmission} />
+                <PostBox />
                 <VStack>
                     {renderAllCommunityPosts()}
                 </VStack>
