8eaa7d2234da950c14982e312d5a88065286a8d1
HENERALPEPITO <95832346+HENERALPEPITO@users.noreply.github.com>
2024-04-19 20:38:50 +0800

    FIREBASE ISKLUTTER DB
    New FIREBASE Config

--- config/firebase.js
+++ config/firebase.js
@@ -6,13 +6,13 @@ import { getStorage, ref as storageRef, uploadBytes } from 'firebase/storage';
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyAl1jm8q5onXVIwWQI-OVLxviw9lpda2-A",
-  authDomain: "isklutterfinal.firebaseapp.com",
-  projectId: "isklutterfinal",
-  storageBucket: "isklutterfinal.appspot.com",
-  messagingSenderId: "1099286368526",
-  appId: "1:1099286368526:web:1c8d4b34ae2c958b07875f",
-  measurementId: "G-0B2GX1SR93"
+  apiKey: "AIzaSyDrcqUw30yuHuUc0wDiB2xd--YvFkSIV9Q",
+  authDomain: "isklutterfirebase.firebaseapp.com",
+  projectId: "isklutterfirebase",
+  storageBucket: "isklutterfirebase.appspot.com",
+  messagingSenderId: "442462941859",
+  appId: "1:442462941859:web:99102ff81ef333801a8516",
+  measurementId: "G-KKS642S9J4"
 };
 
 const app = initializeApp(firebaseConfig);
