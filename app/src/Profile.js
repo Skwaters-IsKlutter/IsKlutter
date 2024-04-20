861164c0548245af4d154730eaf6f51b75654c51
HENERALPEPITO <95832346+HENERALPEPITO@users.noreply.github.com>
2024-04-20 10:21:51 +0800

    FILTER MY LISTINGS to MY BIDDINGS
    FILTER

--- app/src/Profile.js
+++ app/src/Profile.js
@@ -30,147 +30,215 @@ export default function ProfilePage() {
     const [username, setUsername] = React.useState('');
     const [loadingProfile, setLoadingProfile] = React.useState(true);
     const [bio, setBio] = React.useState('');
-    const [userListings, setUserListings] = React.useState([]);
+const [userListings, setUserListings] = React.useState({ myListings: [], myBiddings: [] });
+    
 
     useFocusEffect(
         React.useCallback(() => {
           const fetchData = async () => {
             const user = auth.currentUser;
             if (user !== null) {
-              setLoadingProfile(true);
-              const q = query(collection(database, 'users'), where('userID', '==', user.uid));
-              const querySnapshot = await getDocs(q);
-    
-              if (!querySnapshot.empty) {
-                const userDoc = querySnapshot.docs[0];
-                const userDocSnapshot = await getDoc(doc(database, userDoc.ref.path));
-    
-                if (userDocSnapshot.exists()) {
-                  const userData = userDocSnapshot.data();
-                  setCurrentUser(userData);
-
-                  // Set the userProfileImg URL
-                  const userProfileImg = userData?.userProfileImg || '';
-                  setProfileImg(userProfileImg);
+                setLoadingProfile(true);
+                const q = query(collection(database, 'users'), where('userID', '==', user.uid));
+                const querySnapshot = await getDocs(q);
+        
+                if (!querySnapshot.empty) {
+                    const userDoc = querySnapshot.docs[0];
+                    const userDocSnapshot = await getDoc(doc(database, userDoc.ref.path));
+        
+                    if (userDocSnapshot.exists()) {
+                        const userData = userDocSnapshot.data();
+                        setCurrentUser(userData);
+        
+                        // Set the userProfileImg URL
+                        const userProfileImg = userData?.userProfileImg || '';
+                        setProfileImg(userProfileImg);
+                    } else {
+                        console.log('User document does not exist.');
+                    }
                 } else {
-                  console.log('User document does not exist.');
+                    console.log('User document not found.');
                 }
-              } else {
-                console.log('User document not found.');
-              }
-
-              // Fetch user listings
-              const userListingQuery = query(collection(database, 'listings'), where('sellerID', '==', user.uid));
-              const userListingQuerySnapshot = await getDocs(userListingQuery);
-              const userListingData = userListingQuerySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-              setUserListings(userListingData);
-    
-              setLoadingProfile(false);
+        
+                // Fetch user listings where 'sellerID' is equal to user.uid and 'bidding' is false
+                const listingsQuerySnapshot = await getDocs(query(collection(database, 'listings'), 
+                    where('sellerID', '==', user.uid),
+                    where('bidding', '==', false)
+                ));
+                const userListingData = listingsQuerySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        
+                // Fetch user biddings where 'sellerID' is equal to user.uid and 'bidding' is true
+                const biddingQuerySnapshot = await getDocs(query(collection(database, 'listings'), 
+                    where('sellerID', '==', user.uid),
+                    where('bidding', '==', true)
+                ));
+                const userBiddingsData = biddingQuerySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        
+                setUserListings({ myListings: userListingData, myBiddings: userBiddingsData });
+        
+                setLoadingProfile(false);
             }
-          };
-    
-          fetchData(); // Fetch data when the screen comes into focus
+        };
+        
+        fetchData(); 
         }, []) // Empty dependency array means it will only run once when the component mounts
       );
 
       const renderUserListings = () => {
-        return userListings.map((item) => {
-          const firstTag = item.listingTags && item.listingTags.length > 0 ? item.listingTags[0] : null;
-
-          return (
-            <ItemCard
-                key={item.id}
-                productImage={item.listingImageURL}
-                productPrice={item.listingPrice}
-                productName={item.listingName}
-                productSeller={currentUser?.username}
-                tags={firstTag}
-                toListing={() => navigation.navigate(Routes.LISTINGS, { selectedItem: item, sellerImageURL: profileImg, sellerName: currentUser?.username })}
-            />
-          );
-        });
+        return (
+            <>
+                {/* My Listings */}
+                <VStack>
+                    <HStack 
+                        p={5}
+                        alignItems="center"
+                        borderRadius={30} 
+                    >
+                        <MaterialCommunityIcons 
+                            name="view-grid" 
+                            color={colors.secondary} 
+                            size={20}
+                        />
+                        <Heading 
+                            lineHeight={40}
+                            fontSize={20} 
+                            color={colors.secondary} 
+                            pl={10}
+                        >
+                            My Listings
+                        </Heading>
+                    </HStack>
+                    {/* My Listings Container */}
+                    <HStack space="xs" flexWrap="wrap">
+                        {userListings.myListings.map(item => (
+                            <ItemCard
+                                key={item.id}
+                                productImage={item.listingImageURL}
+                                productPrice={item.listingPrice}
+                                productName={item.listingName}
+                                productSeller={currentUser?.username}
+                                tags={item.listingTags && item.listingTags.length > 0 ? item.listingTags[0] : null}
+                                toListing={() => navigation.navigate(Routes.LISTINGS, { selectedItem: item, sellerImageURL: profileImg, sellerName: currentUser?.username })}
+                            />
+                        ))}
+                    </HStack>
+                </VStack>
+    
+                {/* My Biddings */}
+                <VStack>
+                    <HStack 
+                        p={5}
+                        alignItems="center"
+                        borderRadius={30} 
+                    >
+                        <MaterialCommunityIcons 
+                            name="cash-multiple" 
+                            color={colors.secondary} 
+                            size={20}
+                        />
+                        <Heading 
+                            lineHeight={40}
+                            fontSize={20} 
+                            color={colors.secondary} 
+                            pl={10}
+                        >
+                            My Biddings
+                        </Heading>
+                    </HStack>
+                    {/* My Biddings Container */}
+                    <HStack space="xs" flexWrap="wrap">
+                        {userListings.myBiddings.map(item => (
+                            <ItemCard
+                                key={item.id}
+                                productImage={item.listingImageURL}
+                                productPrice={item.listingPrice}
+                                productName={item.listingName}
+                                productSeller={currentUser?.username}
+                                tags={item.listingTags && item.listingTags.length > 0 ? item.listingTags[0] : null}
+                                toListing={() => navigation.navigate(Routes.LISTINGS, { selectedItem: item, sellerImageURL: profileImg, sellerName: currentUser?.username })}
+                            />
+                        ))}
+                    </HStack>
+                </VStack>
+            </>
+        );
     };
+    
 
     return (
-        <Box w="100%" h="100%">
-
-            <SearchHeaderBack userIcon={require("../../assets/img/usericon.jpg")} back={navigation.goBack} />
-            
-            <Box p="$3" w="100%"  flex={1}>
-                <HelloCard  username={currentUser?.username} />
-                <ScrollView>
-                    <ProfileCard
-                        userProfileImg={profileImg}
-                        username={currentUser?.username}
-                        profileName={currentUser?.userProfile || currentUser?.username}
-                        bio={currentUser?.userBio || "I have no interesting info."}
-                        userID={currentUser?.userID}
-                        setProfileName={setProfileName}
-                        setUsername={setUsername}
-                        setBio={setBio}
-                        loading={loadingProfile} // Pass loading state to ProfileCard
-                    />
-                
-                    <Box bgColor="white" 
-                          p="$3"
-                          m={5}
-                        
-                          h="100%" 
-                        
-                    >
-                    <VStack>
-                        <HStack 
-                                 p={5}
-                                  alignItems="center"
-                                  borderRadius={30} >
-                            <MaterialCommunityIcons 
-                                      name="view-grid" 
-                                      color={colors.secondary} 
-                                      size={20}
-                            />
-                            {/* Display user listings */}
-                            <Heading lineHeight={40}
-                                    fontSize={20} 
-                                    color={colors.secondary} 
-                                    pl={10}>
-                                    My Listings
-                                    
+      <Box w="100%" h="100%">
+          <SearchHeaderBack userIcon={require("../../assets/img/usericon.jpg")} back={navigation.goBack} />
+          
+          <Box p="$3" w="100%" flex={1}>
+              <HelloCard  username={currentUser?.username} />
+              <ScrollView>
+                  <ProfileCard
+                      userProfileImg={profileImg}
+                      username={currentUser?.username}
+                      profileName={currentUser?.userProfile || currentUser?.username}
+                      bio={currentUser?.userBio || "I have no interesting info."}
+                      userID={currentUser?.userID}
+                      setProfileName={setProfileName}
+                      setUsername={setUsername}
+                      setBio={setBio}
+                      loading={loadingProfile} // Pass loading state to ProfileCard
+                  />
+              
+                  <Box bgColor="white" p="$3" m={5} h="100%">
+                      <VStack>
+                          <HStack p={5} alignItems="center" borderRadius={30}>
+                              <MaterialCommunityIcons 
+                                  name="view-grid" 
+                                  color={colors.secondary} 
+                                  size={20}
+                              />
+                              <Heading lineHeight={40} fontSize={20} color={colors.secondary} pl={10}>
+                                  My Listings
                               </Heading>
                           </HStack>
-
-                        {/* Listing Box Container */}
-                        
-                            <HStack space="xs" flexWrap="wrap">
-                              {renderUserListings()}
-                            </HStack>
+                          {/* My Listings Container */}
+                          <HStack space="xs" flexWrap="wrap">
+                              {userListings.myListings.map(item => (
+                                  <ItemCard
+                                      key={item.id}
+                                      productImage={item.listingImageURL}
+                                      productPrice={item.listingPrice}
+                                      productName={item.listingName}
+                                      productSeller={currentUser?.username}
+                                      tags={item.listingTags && item.listingTags.length > 0 ? item.listingTags[0] : null}
+                                      toListing={() => navigation.navigate(Routes.LISTINGS, { selectedItem: item, sellerImageURL: profileImg, sellerName: currentUser?.username })}
+                                  />
+                              ))}
+                          </HStack>
                           
-                        
-                          <HStack 
-                                 p={5}
-                                  alignItems="center"
-                                  borderRadius={30} >
-                            <MaterialCommunityIcons 
-                                      name="cash-multiple" 
-                                      color={colors.secondary} 
-                                      size={20}
-                            />
-                            {/* Display user biddings */}
-                            <Heading lineHeight={40}
-                                    fontSize={20} 
-                                    color={colors.secondary} 
-                                    pl={10}>
-                                    My Biddings
-                                    
+                          <HStack p={5} alignItems="center" borderRadius={30}>
+                              <MaterialCommunityIcons 
+                                  name="cash-multiple" 
+                                  color={colors.secondary} 
+                                  size={20}
+                              />
+                              <Heading lineHeight={40} fontSize={20} color={colors.secondary} pl={10}>
+                                  My Biddings
                               </Heading>
                           </HStack>
-
-                          {/* Biddings Container */}
-                        </VStack>
-                     
-                        
-                    </Box>
-                </ScrollView>
-            </Box>
-        </Box>
-    );
-}
+                          {/* My Biddings Container */}
+                          <HStack space="xs" flexWrap="wrap">
+                              {userListings.myBiddings.map(item => (
+                                  <ItemCard
+                                      key={item.id}
+                                      productImage={item.listingImageURL}
+                                      productPrice={item.listingPrice}
+                                      productName={item.listingName}
+                                      productSeller={currentUser?.username}
+                                      tags={item.listingTags && item.listingTags.length > 0 ? item.listingTags[0] : null}
+                                      toListing={() => navigation.navigate(Routes.LISTINGS, { selectedItem: item, sellerImageURL: profileImg, sellerName: currentUser?.username })}
+                                  />
+                              ))}
+                          </HStack>
+                      </VStack>
+                  </Box>
+              </ScrollView>
+          </Box>
+      </Box>
+  );
+}  
