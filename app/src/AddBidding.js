116873e31fa598725966d97e791e756c18f43257
Joshua <gmcarreon@up.edu.ph>
2024-05-16 08:25:36 +0800

    Added UI for Bidding Increments

--- app/src/AddBidding.js
+++ app/src/AddBidding.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     VStack,
     Heading,
@@ -22,14 +22,16 @@ import {
     SelectItem,
     Pressable,
     HStack,
-    Text
+    Text,
+    Input,
+    InputField
 } from '@gluestack-ui/themed';
 import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 
-import { collection, doc, query, where, getDocs, setDoc} from 'firebase/firestore';
-import { storage, storageRef, uploadBytes,  database, auth } from '../../config/firebase';
+import { collection, doc, query, where, getDocs, setDoc } from 'firebase/firestore';
+import { storage, storageRef, uploadBytes, database, auth } from '../../config/firebase';
 import { getDownloadURL } from 'firebase/storage';
 
 import AddListingBox from '../components/AddListingBox.js';
@@ -46,12 +48,13 @@ export default function AddListingPage() {
         listingDescription: '',
         listingTags: [],
         bidding: true,
-       
+
     });
     const [username, setUsername] = useState('');
     const [isFirestoreOnline, setIsFirestoreOnline] = useState(true);
     const [loading, setLoading] = useState(false);
     const [selectedDays, setSelectedDays] = useState(1);
+    const [bidIncrement, setBidIncrement] = useState('');
 
     useEffect(() => {
         fetchUsername();
@@ -89,28 +92,28 @@ export default function AddListingPage() {
         console.log("Selected days:", value); // Add this line to log the selected value
     };
 
-  
+
 
     const handlePostNow = async () => {
         try {
             setLoading(true);
-    
+
             if (!isFirestoreOnline) {
                 Alert.alert("Error", "Please check your internet connection.");
                 throw new Error('Firestore is currently offline. Please check your internet connection and try again.');
             }
-    
+
             if (!listingData.listingName || !listingData.listingPrice || !listingData.listingDescription || !listingData.listingImage) {
                 Alert.alert("Error", "Please fill in all required fields.");
                 throw new Error('Please fill in all required fields (Listing Title, Price, Description, and Image).');
             }
-    
+
             const listingPrice = parseFloat(listingData.listingPrice);
             if (isNaN(listingPrice)) {
                 Alert.alert("Error", "Listing price must be a number.");
                 throw new Error('Listing price must be a valid number.');
             }
-    
+
             const user = auth.currentUser;
             const uid = user.uid;
             const docRef = doc(collection(database, 'listings'));
@@ -123,11 +126,11 @@ export default function AddListingPage() {
             const metadata = { contentType: 'image/jpeg' };
             await uploadBytes(imageRef, file, metadata);
             const downloadURL = await getDownloadURL(imageRef);
-    
+
             // Calculate timestamp based on selected bidding time
             const currentDate = new Date();
             const adjustedTimestamp = new Date(currentDate.setDate(currentDate.getDate() + selectedDays));
-    
+
             const listingDataFirestore = {
                 sellerID: uid,
                 key: newListingId,
@@ -136,9 +139,10 @@ export default function AddListingPage() {
                 listingPrice: listingPrice,
                 listingImage: storagePath,
                 listingImageURL: downloadURL,
-                endTime: adjustedTimestamp 
+                endTime: adjustedTimestamp,
+                bidIncrement: bidIncrement,
             };
-    
+
             await setDoc(docRef, listingDataFirestore);
             setListingData({
                 listingImage: null,
@@ -147,7 +151,7 @@ export default function AddListingPage() {
                 listingDescription: '',
                 listingTags: [],
                 bidding: true,
-             
+
             });
             navigation.goBack();
         } catch (error) {
@@ -156,15 +160,15 @@ export default function AddListingPage() {
             setLoading(false);
         }
     };
-    
+
 
     const handleCancel = () => {
         navigation.goBack();
     };
 
-    return (    
+    return (
         <Box w="100%" h="100%">
-           {/* Header */}
+            {/* Header */}
             <Box backgroundColor={colors.primary}>
                 <HStack p="$2" w="100%" mt={25} alignItems="center" >
                     <Pressable onPress={navigation.goBack}>
@@ -174,18 +178,18 @@ export default function AddListingPage() {
                 </HStack>
             </Box>
 
-      
+
             <Box w="100%" maxWidth="$96" flex={1}>
-                 <ScrollView>
+                <ScrollView>
                     <Box>
                         <VStack space="xs">
                             <AddBiddingBox
-                                    listingImage={require('../../assets/img/item.jpg')}
-                                    listingName={listingData.listingName}
-                                    listingPrice={listingData.listingPrice}
-                                    listingDescription={listingData.listingDescription}
-                                    listingTags={listingData.listingTags}
-                                    setListingData={setListingData}
+                                listingImage={require('../../assets/img/item.jpg')}
+                                listingName={listingData.listingName}
+                                listingPrice={listingData.listingPrice}
+                                listingDescription={listingData.listingDescription}
+                                listingTags={listingData.listingTags}
+                                setListingData={setListingData}
                             />
                         </VStack>
                     </Box>
@@ -196,46 +200,63 @@ export default function AddListingPage() {
                                 <SelectInput placeholder="Select Time" />
                             </SelectTrigger>
                             <SelectPortal>
-                                 <SelectBackdrop />
-                                    <SelectContent>
-                                        <SelectDragIndicatorWrapper>
-                                            <SelectDragIndicator />
-                                        </SelectDragIndicatorWrapper>
-                                                <SelectItem label="1 day" value={1} />
-                                                <SelectItem label="2 days" value={2} />
-                                                <SelectItem label="3 days" value={3} />
-                                                <SelectItem label="4 days" value={4} />
-                                                <SelectItem label="5 days" value={5} />
-                                    </SelectContent>
+                                <SelectBackdrop />
+                                <SelectContent>
+                                    <SelectDragIndicatorWrapper>
+                                        <SelectDragIndicator />
+                                    </SelectDragIndicatorWrapper>
+                                    <SelectItem label="1 day" value={1} />
+                                    <SelectItem label="2 days" value={2} />
+                                    <SelectItem label="3 days" value={3} />
+                                    <SelectItem label="4 days" value={4} />
+                                    <SelectItem label="5 days" value={5} />
+                                </SelectContent>
                             </SelectPortal>
-                            </Select>
+                        </Select>
+                    </Box>
+
+                    <Box p={10} top={-30}>
+                        <FormControl >
+                            <FormControlLabel mb="$1">
+                                <FormControlLabelText color={colors.secondary} fontWeight={600}>Bidding Increment</FormControlLabelText>
+                            </FormControlLabel>
+                            <Input w="100%" bg={colors.white} h='$10' borderRadius={10}>
+                                <InputField
+                                    value={bidIncrement}
+                                    onChangeText={(text) => setBidIncrement(text)}
+                                    keyboardType="numeric"
+                                    placeholder="Enter bidding increment"
+                                />
+                            </Input>
+                        </FormControl>
+
                     </Box>
 
                     <HStack p="$2" justifyContent="center" mb={10}>
-                            <Button
-                                variant="solid"
-                                size="md"
-                                bg={colors.secondary}
-                                borderRadius={10}
-                                onPress={handlePostNow}
-                                disabled={loading}
-                            >
-                                <ButtonText color={colors.white} fontSize="$md">
-                                    {loading ? 'Posting' : 'Post Now'}
-                                </ButtonText>
-                            </Button>
-                            
-                            <Button
-                                variant="solid"
-                                size="md"
-                                bg={colors.gray}
-                                borderRadius={10}
-                                ml={10}
-                                onPress={handleCancel}>
-                                <ButtonText color={colors.white} fontSize="$md">
-                                    Cancel
-                                </ButtonText>
-                            </Button>
+                        <Button
+                            variant="solid"
+                            size="md"
+                            bg={colors.secondary}
+                            borderRadius={10}
+                            onPress={handlePostNow}
+                            disabled={loading}
+                        >
+                            <ButtonText color={colors.white} fontSize="$md">
+                                {loading ? 'Posting' : 'Post Now'}
+                            </ButtonText>
+                        </Button>
+
+                        <Button
+                            variant="solid"
+                            size="md"
+                            bg={colors.gray}
+                            borderRadius={10}
+                            ml={10}
+                            onPress={handleCancel}>
+                            <ButtonText color={colors.white} fontSize="$md">
+                                Cancel
+                            </ButtonText>
+                        </Button>
                     </HStack>
                 </ScrollView>
             </Box>
